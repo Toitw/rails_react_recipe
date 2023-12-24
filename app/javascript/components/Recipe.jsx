c5684afa98b51d362ff87e53e24325e97a27398d
juroga_new <juanjrgast@gmail.com>
2023-12-24 17:24:37 +0100

    Add deleteRecipe function to Recipe component

--- app/javascript/components/Recipe.jsx
+++ app/javascript/components/Recipe.jsx
@@ -41,6 +41,28 @@ const Recipe = () => {
     
     const recipeInstruction = recipe && recipe.instruction ? addHtmlEntities(recipe.instruction) : '';
 
+    const deleteRecipe = () => {
+        const url = `/api/v1/destroy/${params.id}`;
+        const token = document.querySelector('meta[name="csrf-token"]').content;
+    
+        fetch(url, {
+          method: "DELETE",
+          headers: {
+            "X-CSRF-Token": token,
+            "Content-Type": "application/json",
+          },
+        })
+          .then((response) => {
+            if (response.ok) {
+              return response.json();
+            }
+            throw new Error("Network response was not ok.");
+          })
+          .then(() => navigate("/recipes"))
+          .catch((error) => console.log(error.message));
+    };
+
+
     return (
         <div className="">
           <div className="hero position-relative d-flex align-items-center justify-content-center">
@@ -72,6 +94,7 @@ const Recipe = () => {
               </div>
               <div className="col-sm-12 col-lg-2">
                 <button
+                  onClick={deleteRecipe}
                   type="button"
                   className="btn btn-danger"
                 >
